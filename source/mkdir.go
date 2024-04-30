a807dbd4ebab9c57ae2b9c67ee37d00ef276a6ef
pixelkingliam <79420307+pixelkingliam@users.noreply.github.com>
2024-04-30 19:21:01 -0400

    mkdir -> Added verbose argument and

--- source/mkdir.go
+++ source/mkdir.go
@@ -11,6 +11,7 @@ func main() {
 	var options struct {
 		Parents bool `short:"p" long:"parents" description:"No errors if existing, also creates necessary parent directories as needed."` // GNU Compatible
 		Mode    int  `short:"m" long:"mode" description:"Set file mode (numerical representation)" default:"0755"`                        // GNU Compatible
+		Verbose bool `short:"v" long:"verbose" description:"Print a message for every directories created"`                               // GNU Compatible
 	}
 	options.Mode = 0755
 	args, err := flags.ParseArgs(&options, os.Args)
@@ -26,14 +27,19 @@ func main() {
 		fmt.Println("Missing operand.")
 		os.Exit(1)
 	}
-	dir := args[0]
-	if options.Parents {
-		err = os.MkdirAll(dir, os.FileMode(options.Mode))
-	} else {
-		err = os.Mkdir(dir, os.FileMode(options.Mode))
-	}
-	if err != nil {
-		fmt.Println(`Unexpected error:`, err.Error())
-		os.Exit(1)
+	for i := 0; i < len(args); i++ {
+		dir := args[i]
+		if options.Parents {
+			err = os.MkdirAll(dir, os.FileMode(options.Mode))
+		} else {
+			err = os.Mkdir(dir, os.FileMode(options.Mode))
+		}
+		if err != nil {
+			fmt.Println(`Unexpected error:`, err.Error())
+			os.Exit(1)
+		} else if options.Verbose {
+			fmt.Printf("Creating directory %s.\n", dir)
+		}
+
 	}
 }
