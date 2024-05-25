8bbd080a559b48a3ec84ee8b172818fc573c86c0
pixelkingliam <79420307+pixelkingliam@users.noreply.github.com>
2024-05-24 21:01:58 -0400

    byte -> more fixes to make it work

--- shared/convert/byte.go
+++ shared/convert/byte.go
@@ -64,35 +64,37 @@ func ToPeta[T constraints.Integer](value T, options ...bool) float32 {
 		return float32(value) / (1024 * 1024 * 1024 * 1024 * 1024)
 	}
 }
-func ToBinary(value int64) string {
+func ToBinary[T constraints.Integer](value T) string {
+	finalValue := uint64(value)
 	switch {
-	case value >= (1024 * 1024 * 1024 * 1024 * 1024):
-		return fmt.Sprintf("%v PiB", ToPeta(value))
-	case value >= (1024 * 1024 * 1024 * 1024):
-		return fmt.Sprintf("%v TiB", ToTera(value))
-	case value >= (1024 * 1024 * 1024):
-		return fmt.Sprintf("%v GiB", ToGiga(value))
-	case value >= (1024 * 1024):
-		return fmt.Sprintf("%v MiB", ToMega(value))
-	case value >= 1024:
-		return fmt.Sprintf("%v KiB", ToKilo(value))
+	case finalValue >= (1024 * 1024 * 1024 * 1024 * 1024):
+		return fmt.Sprintf("%v PiB", ToPeta(finalValue))
+	case finalValue >= (1024 * 1024 * 1024 * 1024):
+		return fmt.Sprintf("%v TiB", ToTera(finalValue))
+	case finalValue >= (1024 * 1024 * 1024):
+		return fmt.Sprintf("%v GiB", ToGiga(finalValue))
+	case finalValue >= (1024 * 1024):
+		return fmt.Sprintf("%v MiB", ToMega(finalValue))
+	case finalValue >= 1024:
+		return fmt.Sprintf("%v KiB", ToKilo(finalValue))
 	default:
-		return fmt.Sprintf("%v B", value)
+		return fmt.Sprintf("%v B", finalValue)
 	}
 }
 
-func ToSI(value int64) string {
+func ToSI[T constraints.Integer](value T) string {
+	finalValue := uint64(value)
 	switch {
-	case value >= 1000000000000000:
-		return fmt.Sprintf("%v PB", ToPeta(value, true))
-	case value >= 1000000000000:
-		return fmt.Sprintf("%v TB", ToTera(value, true))
-	case value >= 1000000000:
-		return fmt.Sprintf("%v GB", ToGiga(value, true))
-	case value >= 1000000:
-		return fmt.Sprintf("%v MB", ToMega(value, true))
-	case value >= 1000:
-		return fmt.Sprintf("%v KB", ToKilo(value, true))
+	case finalValue >= 1000000000000000:
+		return fmt.Sprintf("%v PB", ToPeta(finalValue, true))
+	case finalValue >= 1000000000000:
+		return fmt.Sprintf("%v TB", ToTera(finalValue, true))
+	case finalValue >= 1000000000:
+		return fmt.Sprintf("%v GB", ToGiga(finalValue, true))
+	case finalValue >= 1000000:
+		return fmt.Sprintf("%v MB", ToMega(finalValue, true))
+	case finalValue >= 1000:
+		return fmt.Sprintf("%v KB", ToKilo(finalValue, true))
 	default:
 		return fmt.Sprintf("%v B", value)
 	}
