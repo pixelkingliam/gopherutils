927124db2f6873cdfd24202cad7d69a74b92ac67
pixelkingliam <79420307+pixelkingliam@users.noreply.github.com>
2024-06-01 19:59:18 -0400

    printenv -> added printenv

---
+++ source/printenv.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/jessevdk/go-flags"
+	"os"
+)
+
+func main() {
+	var options struct {
+		Null bool `short:"0" long:"null" description:"Ends each output line with NUL instead of a newline."`
+	}
+	_, err := flags.ParseArgs(&options, os.Args[1:])
+	if err != nil {
+		if errors.Is(err, flags.ErrHelp) {
+			os.Exit(0)
+		} else {
+			os.Exit(1)
+		}
+	}
+	ending := '\n'
+	for _, str := range os.Environ() {
+		if str[:2] != "_=" {
+			fmt.Printf("%s%s", str, ending)
+
+		}
+	}
+}
