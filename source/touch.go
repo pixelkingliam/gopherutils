a80126c8a0964aec815f8d9a5819bd06cd4529ec
pixelkingliam <79420307+pixelkingliam@users.noreply.github.com>
2024-06-14 23:22:10 -0400

    touch -> added basic functionality

---
+++ source/touch.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/jessevdk/go-flags"
+	"os"
+	"time"
+)
+
+func main() {
+	var options struct {
+	}
+	args, err := flags.ParseArgs(&options, os.Args)
+
+	if err != nil {
+		if errors.Is(err, flags.ErrHelp) {
+			os.Exit(0)
+		} else {
+			os.Exit(1)
+		}
+	}
+	if len(args) == 1 {
+		fmt.Println("Missing file operand")
+		os.Exit(1)
+	}
+	args = args[1:]
+
+	for _, arg := range args {
+		_, err := os.Stat(arg)
+		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				_, err2 := os.Create(arg)
+				if err2 != nil {
+					fmt.Printf("Unknown error: %s\n", err.Error())
+					os.Exit(1)
+				}
+			} else {
+				fmt.Printf("Unknown error: %s\n", err.Error())
+				os.Exit(1)
+			}
+		}
+		err = os.Chtimes(arg, time.Now(), time.Now())
+		if err != nil {
+			fmt.Printf("Unknown error: %s\n", err.Error())
+			os.Exit(1)
+		}
+
+	}
+
+}
