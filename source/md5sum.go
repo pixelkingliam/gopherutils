39f511d15f4d248086ba08e797ad2f913c52796f
pixelkingliam <79420307+pixelkingliam@users.noreply.github.com>
2024-07-11 13:39:54 -0400

    md5sum -> added -b

--- source/md5sum.go
+++ source/md5sum.go
@@ -10,7 +10,8 @@ import (
 
 func main() {
 	var options struct {
-		Zero bool `short:"z" long:"zero" description:"Ends each output with a NUL character instead of a newline character."` // GNU Compatible
+		Zero   bool `short:"z" long:"zero" description:"Ends each output with a NUL character instead of a newline character."` // GNU Compatible
+		Binary bool `short:"b" long:"binary" description:"Reads in binary mode, does nothing on GNU systems."`                  // GNU Compatible
 	}
 	args, err := flags.ParseArgs(&options, os.Args)
 	if len(args) != 0 {
@@ -28,6 +29,10 @@ func main() {
 	if options.Zero {
 		ending = "\x00"
 	}
+	prefix := " "
+	if options.Binary {
+		prefix = " *"
+	}
 	for _, arg := range args {
 		_, err := os.Stat(arg)
 		if err != nil {
@@ -43,7 +48,7 @@ func main() {
 			os.Exit(1)
 		}
 
-		fmt.Printf("%x%s%s", md5.Sum(file), arg, ending)
+		fmt.Printf("%x%s%s%s", md5.Sum(file), prefix, arg, ending)
 	}
 
 }
