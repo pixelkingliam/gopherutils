63d330e562cb6e41e261766db0c181d0d65dcff4
pixelkingliam <79420307+pixelkingliam@users.noreply.github.com>
2024-07-30 14:42:36 -0400

    shasum -> added

---
+++ source/shasum.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"errors"
+	"fmt"
+	"github.com/jessevdk/go-flags"
+	"os"
+)
+
+func main() {
+	var options struct {
+		Algorithm int  `short:"a" long:"algorithm" description:"Selects the SHA algorithm to use, available options:1 (Default)\n224\n256\n384\n512\n512224\n512256"` // GNU Compatible
+		Binary    bool `short:"b" long:"binary" description:"Reads in binary mode, does nothing on GNU systems."`                                                     // GNU Compatible
+		Text      bool `short:"t" long:"text" description:"Reads in text mode."`                                                                                      // GNU Compatible
+		Tag       bool `short:"T" long:"tag" description:"Writes BSD-style checksums."`                                                                               // GNU Compatible
+
+	}
+	options.Text = true
+	options.Algorithm = 1
+	args, err := flags.ParseArgs(&options, os.Args)
+	if len(args) != 0 {
+		args = args[1:]
+
+	}
+	if err != nil {
+		if errors.Is(err, flags.ErrHelp) {
+			os.Exit(0)
+		} else {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	if !checkAlgo(options.Algorithm) {
+		fmt.Println("Invalid SHA algorithm\nTry 'shasum -h' for help.")
+		os.Exit(1)
+	}
+	if false {
+		// TODO CHECK ARG
+	} else {
+		for i := 0; i < len(args); i++ {
+			file, err := os.ReadFile(args[i])
+			if err != nil {
+				fmt.Println("Error reading file:", err)
+				continue
+			}
+			fmt.Printf("%s%s", formatHash(options.Algorithm, file, options.Tag, args[i]), "\n")
+		}
+	}
+
+}
+
+func checkAlgo(algorithm int) bool {
+	switch algorithm {
+	case 1:
+		return true
+	case 224:
+		return true
+	case 256:
+		return true
+	case 384:
+		return true
+	case 512:
+		return true
+	case 512224:
+		return true
+	case 512256:
+		return true
+	default:
+		return false
+
+	}
+}
+func algoString(algorithm int) string {
+	switch algorithm {
+	case 1:
+		return "SHA1"
+	case 224:
+		return "SHA224"
+	case 256:
+		return "SHA256"
+	case 384:
+		return "SHA384"
+	case 512:
+		return "SHA512"
+	case 512224:
+		return "SHA512/224"
+	case 512256:
+		return "SHA512/256"
+	default:
+		return "UNKNOWN"
+
+	}
+}
+func getHash(algorithm int, data []byte) (string, error) {
+	switch algorithm {
+	case 1:
+		return fmt.Sprintf("%x", sha1.Sum(data)), nil
+	case 224:
+		return fmt.Sprintf("%x", sha256.Sum224(data)), nil
+	case 256:
+		return fmt.Sprintf("%x", sha256.Sum256(data)), nil
+	case 384:
+		return fmt.Sprintf("%x", sha512.Sum384(data)), nil
+	case 512:
+		return fmt.Sprintf("%x", sha512.Sum512(data)), nil
+	case 512224:
+		return fmt.Sprintf("%x", sha512.Sum512_224(data)), nil
+	case 512256:
+		return fmt.Sprintf("%x", sha512.Sum512_256(data)), nil
+	default:
+		return "", errors.New("invalid SHA algorithm")
+
+	}
+}
+func formatHash(algorithm int, data []byte, tag bool, fileName string) string {
+	hash, err := getHash(algorithm, data)
+	if err != nil {
+		fmt.Printf("Error getting hash for %s: %s\n", fileName, err)
+		os.Exit(1)
+	}
+	if tag {
+		return fmt.Sprintf("%s (%s) = %s", algoString(algorithm), fileName, hash)
+	} else {
+		return fmt.Sprintf("%s  %s", hash, fileName)
+	}
+}
