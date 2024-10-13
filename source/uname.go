b2df2cd839b628553ccaa696b5d2b4a18de0f682
pixelkingliam <79420307+pixelkingliam@users.noreply.github.com>
2024-10-12 20:34:43 -0400

    uname -> added -o

--- source/uname.go
+++ source/uname.go
@@ -16,13 +16,14 @@ import (
 func main() {
 
 	var options struct {
-		KernelName       bool `short:"s" long:"kernel-name" description:"Prints the kernel's name'"`               // GNU Compatible
-		Hostname         bool `short:"n" long:"nodename" description:"Prints the computer's network name."`        // GNU Compatible
-		Release          bool `short:"r" long:"kernel-release" description:"Prints the kernel's release version."` // GNU Compatible
-		BuildDate        bool `short:"v" description:"Prints the kernel's build date."`                            // GNU Compatible
-		MachineArch      bool `short:"m" long:"machine" description:"Prints the computer's architecture."`         // GNU Compatible
-		Processor        bool `short:"p" long:"processor" description:"Prints the computer's CPU."`                // GNU Incompatible
-		HardwarePlatform bool `short:"i" long:"hardware-platform" description:"Prints the computer's model."`      // GNU Incompatible
+		KernelName       bool `short:"s" long:"kernel-name" description:"Prints the kernel's name'"`                 // GNU Compatible
+		Hostname         bool `short:"n" long:"nodename" description:"Prints the computer's network name."`          // GNU Compatible
+		Release          bool `short:"r" long:"kernel-release" description:"Prints the kernel's release version."`   // GNU Compatible
+		BuildDate        bool `short:"v" description:"Prints the kernel's build date."`                              // GNU Compatible
+		MachineArch      bool `short:"m" long:"machine" description:"Prints the computer's architecture."`           // GNU Compatible
+		Processor        bool `short:"p" long:"processor" description:"Prints the computer's CPU."`                  // GNU Incompatible
+		HardwarePlatform bool `short:"i" long:"hardware-platform" description:"Prints the computer's model."`        // GNU Incompatible
+		OS               bool `short:"o" long:"operating-system" description:"Prints the operating system's name'."` // GNU Incompatible
 		//SafeVArg
 	}
 	args, err := flags.ParseArgs(&options, os.Args)
@@ -110,5 +111,37 @@ func main() {
 		fmt.Print(strings.Replace(system.Name, "\n", "", -1))
 		fmt.Print(" ")
 	}
+	if options.OS {
+		// try getting OS from /etc/os-release
+		stat, err := os.Stat("/etc/os-release")
+		if err == nil {
+			if !stat.IsDir() {
+				file, err := os.ReadFile("/etc/os-release")
+				if err != nil {
+					fmt.Printf("Could not read os-release")
+					return
+				}
+				osName := strings.Split(strings.Split(string(file), "\n")[0], "=")[1]
+				osName = strings.Trim(osName, "\"")
+				fmt.Print(osName)
+			}
+		}
+		// try getting OS info from /etc/lsb-release
+		stat, err = os.Stat("/etc/lsb-release")
+		if err == nil {
+			if !stat.IsDir() {
+				file, err := os.ReadFile("/etc/lsb-release")
+				if err != nil {
+					fmt.Printf("Could not read lsb-release")
+					return
+				}
+				osName := strings.Split(strings.Split(string(file), "\n")[0], "=")[1]
+				osName = strings.Trim(osName, "\"")
+				fmt.Print(osName)
+			}
+		}
+		fmt.Print(" ")
+	}
+
 	fmt.Println("\b")
 }
